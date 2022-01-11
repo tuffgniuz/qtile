ae4a305edb13b3f7d66090b6f704910e8a526251
Imanuel Febie <tuffgniuz.dev@gmail.com>
2022-01-11 17:48:05 +0700

    Nobar

--- config.py
+++ config.py
@@ -1,138 +1,199 @@
 import os
 import subprocess
 
-from libqtile.config import ScratchPad, DropDown, Group, Match, Key
-from typing import List
-from libqtile.config import Match
-from libqtile import hook, layout
+from typing import List  # noqa: F401
 
-from colors import nord_fox
+from libqtile import bar, layout, widget, hook
+from libqtile.layout.columns import Columns
+from libqtile.layout.max import Max
+from libqtile.layout.xmonad import MonadTall
+from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
-from bar import screens, widget_defaults
-from keybindings import browser, terminal, keys, mod, mouse
+from libqtile.utils import guess_terminal
+
+mod = "mod4"
+terminal = guess_terminal()
+
+keys = [
+    # A list of available commands that can be bound to keys can be found
+    # at https://docs.qtile.org/en/latest/manual/config/lazy.html
+
+    # Launch applications
+    Key([mod], "w", lazy.spawn('firefox'), desc="Launch firefox"),
+
+    # Switch between windows
+    Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
+    Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
+    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
+    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
+    Key([mod], "space", lazy.layout.next(),
+        desc="Move window focus to other window"),
+
+    # monadtall layouts
+    Key([mod], "i", lazy.layout.grow()),
+    Key([mod], "m", lazy.layout.shrink()),
+    Key([mod], "n", lazy.layout.normalize()),
+    Key([mod], "o", lazy.layout.maximize()),
+
+
+    # Move windows between left/right columns or move up/down in current stack.
+    # Moving out of range in Columns layout will create new column.
+    Key([mod, "shift"], "h", lazy.layout.shuffle_left(),
+        desc="Move window to the left"),
+    Key([mod, "shift"], "l", lazy.layout.shuffle_right(),
+        desc="Move window to the right"),
+    Key([mod, "shift"], "j", lazy.layout.shuffle_down(),
+        desc="Move window down"),
+    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+
+    # Grow windows. If current window is on the edge of screen and direction
+    # will be to screen edge - window would shrink.
+    Key([mod, "control"], "h", lazy.layout.grow_left(),
+        desc="Grow window to the left"),
+    Key([mod, "control"], "l", lazy.layout.grow_right(),
+        desc="Grow window to the right"),
+    Key([mod, "control"], "j", lazy.layout.grow_down(),
+        desc="Grow window down"),
+    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
+    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+    Key([mod], "f", lazy.window.toggle_fullscreen(),
+        desc="Toggle Fullscreen mode"),
+    Key([mod, "shift"], "space", lazy.window.toggle_floating(),
+        desc="Toggle floating mode"),
+
+    # Toggle between split and unsplit sides of stack.
+    # Split = all windows displayed
+    # Unsplit = 1 window displayed, like Max layout, but still with
+    # multiple stack panes
+    Key([mod, "shift"], "Return", lazy.layout.toggle_split(),
+        desc="Toggle between split and unsplit sides of stack"),
+    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+
+    # Toggle between different layouts as defined below
+    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+    Key([mod, "shift"], "c", lazy.window.kill(), desc="Kill focused window"),
+
+    Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
+    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
+    Key([mod], "r", lazy.spawncmd(),
+        desc="Spawn a command using a prompt widget"),
+]
+
+# groups = [Group(i) for i in "123456789"]
 
 
 groups = [
-    Group("1", label="一", matches=[Match(wm_class=[browser])], layout="max"),
-    Group("2", label="二", matches=[Match(wm_class=["gimp"])]),
-    Group("3", label="三", matches=[
-          Match(wm_class=["DesktopEditors"])], layout="columns"),
-    Group(
-        "4",
-        label="四",
-        matches=[
-            Match(wm_class=["zoom", "discord", "slack"])],
-        layout="max"
-    ),
-    Group("5", label="五", layout="max"),
-    Group("6", label="六"),
-    Group("7", label="七"),
-    Group("8", label="八"),
-    Group("9", label="九"),
+    Group('1', label="一", matches=[Match(wm_class='firefox')], layout="max"),
+    Group('2', label="二"),
+    Group('3', label="三"),
+    Group('4', label="三"),
+    Group('5', label="五"),
+    Group('6', label="六"),
+    Group('7', label="七"),
+    Group('8', label="八"),
+    Group('9', label="九"),
 ]
 
-for group in groups:
-    keys.extend(
-        [
-            Key(
-                [mod],
-                group.name,
-                lazy.group[group.name].toscreen(),
-                desc=f"Switch to group {group.name}",
-            ),
-            Key(
-                [mod, "shift"],
-                group.name,
-                lazy.window.togroup(group.name),
-                desc=f"move focused window to group {group.name}",
-            ),
-        ]
-    )
+for i in groups:
+    keys.extend([
+        # mod1 + letter of group = switch to group
+        Key([mod], i.name, lazy.group[i.name].toscreen(),
+            desc="Switch to group {}".format(i.name)),
 
-# append scratchpad to list of groups
-groups.append(
-    ScratchPad(
-        "scratchpad",
-        [
-            DropDown(
-                "term", f"{terminal}", y=0.1, x=0.25, width=0.5, height=0.6, opacity=1
-            ),
-            DropDown(
-                "bitwarden",
-                "bitwarden-desktop",
-                y=0.1,
-                x=0.25,
-                width=0.5,
-                height=0.6,
-                opacity=1,
-            ),
-        ],
-    )
-)
+        # mod1 + shift + letter of group = switch to & move focused window to group
+        Key([mod, "shift"], i.name, lazy.window.togroup(i.name, switch_group=True),
+            desc="Switch to & move focused window to group {}".format(i.name)),
+        # Or, use below if you prefer not to switch to that group.
+        # # mod1 + shift + letter of group = move focused window to group
+        # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
+        #     desc="move focused window to group {}".format(i.name)),
+    ])
 
 layouts = [
-    # layout.Stack(num_stacks=5),
+    MonadTall(
+        border_normal=nord_fox['black'],
+        border_focus=nord_fox['fg_gutter'],
+        single_border_width=2,
+        single_margin=0,
+        margin=10,
+    ),
+    Columns(
+        border_normal=nord_fox['black'],
+        border_focus=nord_fox['fg_gutter'],
+        border_focus_stack=nord_fox['magenta'],
+        border_normal_stack=nord_fox['black'],
+        border_on_single=2,
+        border_width=2,
+        margin_on_single=0),
+    Max(),
+    # Try more layouts by unleashing below layouts.
+    # layout.Stack(num_stacks=2),
     # layout.Bsp(),
     # layout.Matrix(),
-    layout.MonadTall(
-        margin=10,
-        border_normal=nord_fox["black"],
-        border_focus=nord_fox["fg_gutter"],
-        border_width=2,
-        single_border_width=2,
-        single_margin=0,
-    ),
-    layout.Max(single_border_width=2),
-
-    layout.Columns(
-        margin=10,
-        border_normal=nord_fox["black"],
-        border_focus=nord_fox["fg_gutter"],
-        border_normal_stack=nord_fox["black"],
-        border_focus_stack=nord_fox["magenta"],
-        border_width=2,
-        single_border_width=2,
-        single_margin=0,
-        border_on_single=True,
-    ),
     # layout.MonadWide(),
-    # # layout.RatioTile(),
-    # layout.Tile()
+    # layout.RatioTile(),
+    # layout.Tile(),
     # layout.TreeTab(),
     # layout.VerticalTile(),
     # layout.Zoomy(),
 ]
 
 floating_layout = layout.Floating(
+    border_normal=nord_fox['black'],
+    border_focus=nord_fox['blue'],
+    border_width=3,
     float_rules=[
         *layout.Floating.default_float_rules,
-        Match(wm_class="blueman-manager"),
-        Match(wm_class="nemo"),
-        Match(wm_class="pavucontrol"),
-        Match(wm_class="zoom"),
-        Match(wm_class="xarchiver"),
-        Match(wm_class="bitwarden"),
-        Match(wm_class="qbittorrent"),
-        Match(wm_class="mictray"),
-        Match(wm_class="Msgcompose"),
-        Match(wm_class="confirmreset"),  # gitk
-        Match(wm_class="makebranch"),  # gitk
-        Match(wm_class="maketag"),  # gitk
-        Match(wm_class="ssh-askpass"),  # ssh-askpass
-        Match(title="branchdialog"),  # gitk
-        Match(title="pinentry"),  # GPG key password entry
-    ],
-    border_focus=nord_fox["black"],
-    border_width=2,
+        Match(wm_class='confirmreset'),  # gitk
+        Match(wm_class='makebranch'),  # gitk
+        Match(wm_class='maketag'),  # gitk
+        Match(wm_class='ssh-askpass'),  # ssh-askpass
+        Match(title='branchdialog'),  # gitk
+        Match(title='pinentry'),  # GPG key password entry
+        Match(wm_class='nemo'),
+    ])
+
+widget_defaults = dict(
+    font='TerminessTTF Nerd Font',
+    fontsize=13,
+    padding=3,
 )
+extension_defaults = widget_defaults.copy()
+
+screens = [
+    Screen(
+        top=bar.Bar(
+            [
+                widget.GroupBox(),
+                widget.CurrentLayout(),
+                widget.Prompt(),
+                widget.WindowName(),
+                widget.Clock(format='%Y-%m-%d %a %I:%M %p'),
+                widget.Systray(),
+                widget.QuickExit(),
+            ],
+            background=nord_fox['bg'],
+            size=24,
+            # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
+            # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
+        ),
+    ),
+]
+
+# Drag floating layouts.
+mouse = [
+    Drag([mod], "Button1", lazy.window.set_position_floating(),
+         start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(),
+         start=lazy.window.get_size()),
+    Click([mod], "Button2", lazy.window.bring_to_front())
+]
 
 dgroups_key_binder = None
 dgroups_app_rules = []  # type: List
 follow_mouse_focus = True
-bring_front_click = False
+bring_front_click = 'floating_only'
 cursor_warp = False
-
-
 auto_fullscreen = True
 focus_on_window_activation = "smart"
 reconfigure_screens = True
@@ -141,10 +202,18 @@ reconfigure_screens = True
 # focus, should we respect this or not?
 auto_minimize = True
 
+# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
+# string besides java UI toolkits; you can see several discussions on the
+# mailing lists, GitHub issues, and other WM documentation that suggest setting
+# this string if your java app doesn't work correctly. We may as well just lie
+# and say that we're a working one by default.
+#
+# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
+# java that happens to be on java's whitelist.
 wmname = "LG3D"
 
 
 @hook.subscribe.startup_once
 def autostart():
-    home = os.path.expanduser("~/.config/qtile/autostart.sh")
-    subprocess.call([home])
+    home = os.path.expanduser('~/.config/qtile/autostart.sh')
+    subprocess.run([home])
--- unicodes.py
+++ unicodes.py
@@ -1,13 +1,4 @@
-from libqtile.widget import TextBox
-
-
-def lower_left_triangle(bg_color, fg_color):
-    return TextBox(
-        text='\u25e2',
-        padding=0,
-        fontsize=50,
-        background=bg_color,
-        foreground=fg_color)
+from libqtile.widget.textbox import TextBox
 
 
 def left_arrow(bg_color, fg_color):
